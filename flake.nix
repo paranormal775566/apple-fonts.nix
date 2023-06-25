3d4be1dce672c1d9f2dd54df199033a71563818f
paranormal775566 <131408352+paranormal775566@users.noreply.github.com>
2023-06-25 12:05:41 -0400

    Update flake.nix
    Lock github:numtide/flake-utils dependency to stable revision
    Remove unused fonts

--- flake.nix
+++ flake.nix
@@ -1,26 +1,15 @@
 {
   inputs = {
-    flake-utils.url = "github:numtide/flake-utils";
+    flake-utils.url = "github:numtide/flake-utils/abfb11bd1aec8ced1c9bb9adfe68018230f4fb3c";
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
 
     sf-pro = {
       url = "https://devimages-cdn.apple.com/design/resources/download/SF-Pro.dmg";
       flake = false;
     };
-    sf-compact = {
-      url = "https://devimages-cdn.apple.com/design/resources/download/SF-Compact.dmg";
-      flake = false;
-    };
+
     sf-mono = {
       url = "https://devimages-cdn.apple.com/design/resources/download/SF-Mono.dmg";
-      flake = false;
-    };
-    sf-arabic = {
-      url = "https://devimages-cdn.apple.com/design/resources/download/SF-Arabic.dmg";
-      flake = false;
-    };
-    ny = {
-      url = "https://devimages-cdn.apple.com/design/resources/download/NY.dmg";
       flake = false;
     };
   };
@@ -74,17 +63,8 @@
         sf-pro = makeAppleFont "sf-pro" "SF Pro Fonts.pkg" sf-pro;
         sf-pro-nerd = makeNerdAppleFont "sf-pro-nerd" "SF Pro Fonts.pkg" sf-pro;
 
-        sf-compact = makeAppleFont "sf-compact" "SF Compact Fonts.pkg" sf-compact;
-        sf-compact-nerd = makeNerdAppleFont "sf-compact-nerd" "SF Compact Fonts.pkg" sf-compact;
-
         sf-mono = makeAppleFont "sf-mono" "SF Mono Fonts.pkg" sf-mono;
         sf-mono-nerd = makeNerdAppleFont "sf-mono-nerd" "SF Mono Fonts.pkg" sf-mono;
-
-        sf-arabic = makeAppleFont "sf-arabic" "SF Arabic Fonts.pkg" sf-arabic;
-        sf-arabic-nerd = makeNerdAppleFont "sf-arabic-nerd" "SF Arabic Fonts.pkg" sf-arabic;
-
-        ny = makeAppleFont "ny" "NY Fonts.pkg" ny;
-        ny-nerd = makeNerdAppleFont "ny-nerd" "NY Fonts.pkg" ny;
       };
     }
   );
